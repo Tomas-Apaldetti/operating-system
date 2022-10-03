67d3685c9d53e75c6c3949a4cbaf9f6a00fd14bd
Luciano Martin Gamberale <lgamberale@fi.uba.ar>
2022-10-02 21:09:49 -0300

    Comienzo de malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -5,20 +5,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <sys/mman.h>
+
 #include "malloc.h"
 #include "printfmt.h"
 
 #define ALIGN4(s) (((((s) -1) >> 2) << 2) + 4)
 #define REGION2PTR(r) ((r) + 1)
-#define PTR2REGION(ptr) ((struct region *) (ptr) -1)
+#define PTR2REGION(ptr) ((region_t *) (ptr) -1)
 
-struct region {
+#define KiB 1024
+
+#define MIN_REGION_LEN 64
+
+#define BLOCK_LEN_1 (16 * KiB)
+
+#define EXIT_OK 0
+#define EXIT_ERROR -1
+
+#define NEWREGION(region, size) ()
+
+
+// 8  -> 7 -> 0111 -> 0001 -> 0100 = 4 -> 8
+// 10 -> 9 -> 1001 -> 0010 -> 1000 = 8 -> 12
+// 11 -> 10 -> 1010 -> 1000 = 8 	   -> 12
+
+typedef struct region {
 	bool free;
 	size_t size;
 	struct region *next;
-};
+} region_t;
 
-struct region *region_free_list = NULL;
+region_t *region_free_list = NULL;
 
 int amount_of_mallocs = 0;
 int amount_of_frees = 0;
@@ -32,34 +50,49 @@ print_statistics(void)
 	printfmt("requested: %d\n", requested_memory);
 }
 
+
+static region_t *
+find_region_first_fit(size_t size)
+{
+	region_t *actual_region = region_free_list;
+
+	while (actual_region) {
+		if ((actual_region->size >= size) && (actual_region->free)) {
+			return actual_region;
+		}
+		actual_region = actual_region->next;
+	}
+
+	return NULL;
+}
+
 // finds the next free region
 // that holds the requested size
 //
-static struct region *
+static region_t *
 find_free_region(size_t size)
 {
-	struct region *next = region_free_list;
-
 #ifdef FIRST_FIT
 	// Your code here for "first fit"
+	return find_region_first_fit(size);
 #endif
 
 #ifdef BEST_FIT
 	// Your code here for "best fit"
 #endif
 
-	return next;
+	return find_region_first_fit(size);
 }
 
-static struct region *
+/// ANTERIOR
+static region_t *
 grow_heap(size_t size)
 {
 	// finds the current heap break
-	struct region *curr = (struct region *) sbrk(0);
+	region_t *curr = (region_t *) sbrk(0);
 
 	// allocates the requested size
-	struct region *prev =
-	        (struct region *) sbrk(sizeof(struct region) + size);
+	region_t *prev = (region_t *) sbrk(sizeof(region_t) + size);
 
 	// verifies that the returned address
 	// is the same that the previous break
@@ -70,7 +103,7 @@ grow_heap(size_t size)
 	// is successful
 	//
 	// (ref: sbrk(2))
-	if (curr == (struct region *) -1) {
+	if (curr == (region_t *) -1) {
 		return NULL;
 	}
 
@@ -87,29 +120,193 @@ grow_heap(size_t size)
 	return curr;
 }
 
+
+// NUEVO
+
+// EXTRA
+
+void
+print_list()
+{
+	region_t *actual_region = region_free_list;
+	printfmt("\n");
+	printfmt("LISTA:\n");
+	while (actual_region) {
+		if (actual_region->free)
+			printfmt("Direccion: %p - Tamaño: %d - Free\n",
+			         actual_region,
+			         actual_region->size);
+		else
+			printfmt("Direccion: %p - Tamaño: %d - Not Free\n",
+			         actual_region,
+			         actual_region->size);
+
+		actual_region = actual_region->next;
+	}
+	printfmt("\n");
+}
+
+
+// ============================================
+
+// MALLOC
+
+/// @brief Construye un nuevo bloque dentro del Heap.
+/// @param size PARA LA PARTE 2
+/// @return Puntero al bloque creado o NULL en caso de error.
+region_t *
+new_block(size_t size)
+{
+	region_t *new_block;
+	int block_size = BLOCK_LEN_1;
+
+	new_block = mmap(NULL,
+	                 block_size,
+	                 PROT_READ | PROT_WRITE,
+	                 MAP_ANON | MAP_PRIVATE,
+	                 -1,
+	                 0);
+	if (new_block == MAP_FAILED)
+		return NULL;
+
+	new_block->size = block_size - sizeof(region_t);
+	new_block->next = NULL;
+	new_block->free = true;
+
+	return new_block;
+}
+
+/// @brief Appendear el bloque al final de la lista recursivamente
+/// @param actual_region Region actual
+/// @param block Bloque a appendear
+/// @return
+region_t *
+append_block(region_t *actual_region, region_t *block)
+{
+	if (!actual_region) {
+		return block;
+	}
+
+	actual_region = append_block(actual_region->next, block);
+	return actual_region;
+}
+
+/// @brief Agrega el bloque al final de la lista de regiones.
+/// @param block_to_add Bloque a agregar en la lista.
+void
+add_block_to_region_free_list(region_t *block_to_add)
+{
+	region_free_list = append_block(region_free_list, block_to_add);
+}
+
+/// @brief Realiza un split en una determinada region.
+/// @param region Region a la cual se le aplicará el split.
+/// @param size Tamaño que debe tener la región que luego se le retornará al usuario (Region 1).
+void
+try_split_region(region_t *region, size_t size)
+{
+	region_t *region_1 = region;
+	region_t *region_2;
+
+	size_t region_1_size = size + sizeof(region_t);
+	size_t region_2_size = region_1->size - size;
+
+	// Calculo el minimo espacio que debe quedar para la region que quedará
+	// libre. El espacio que debe quedar debe ser de al menos el tamaño de
+	// una region_t + 1 byte. Si eso no se cumple, no le puedo hacer split.
+
+	if (region_2_size <= sizeof(region_t)) {
+		return;
+	}
+
+	region_2 = (region_t *) (((char *) region_1) + region_1_size);
+
+	region_2->next = region_1->next;
+	region_2->size = region_2_size - sizeof(region_t);
+	region_2->free = true;
+
+	region_1->next = region_2;
+	region_1->size = region_1_size - sizeof(region_t);
+	region_1->free = true;
+}
+
+// ============================================
+
+// FREE
+
+void
+coalesce_regions(region_t *region_1, region_t *region_2)
+{
+	region_1->next = region_2->next;
+	region_1->size = region_1->size + region_2->size + sizeof(region_t);
+}
+
+bool
+should_be_coalsce(region_t *region_1, region_t *region_2)
+{
+	region_t *expected_ptr;
+
+	if (!region_2)
+		return false;
+
+	if ((!region_1->free) || (!region_2->free))
+		return false;
+
+	expected_ptr = (region_t *) ((char *) (region_1 + 1) + region_1->size);
+	return (expected_ptr == region_2);
+}
+
+void
+try_coalesce_regions()
+{
+	region_t *previous_region = NULL;
+	region_t *region = region_free_list;
+
+	while (region) {
+		if (should_be_coalsce(region, region->next)) {
+			coalesce_regions(region, region->next);
+		}
+		region = region->next;
+	}
+}
+
+// ============================================
+
 void *
 malloc(size_t size)
 {
-	struct region *next;
+	region_t *block;
+	region_t *region;
 
 	// aligns to multiple of 4 bytes
 	size = ALIGN4(size);
+
+	if (size < MIN_REGION_LEN) {
+		size = MIN_REGION_LEN;
+	}
 
 	// updates statistics
 	amount_of_mallocs++;
 	requested_memory += size;
 
-	next = find_free_region(size);
+	region = find_free_region(size);
+	if (!region) {
+		block = new_block(size);
+		if (!block)
+			return NULL;
 
-	if (!next) {
-		next = grow_heap(size);
+		add_block_to_region_free_list(block);
+
+		region = block;
 	}
 
-	// Your code here
-	//
-	// hint: maybe split free regions?
+	try_split_region(region, size);
+	region->free = false;
 
-	return REGION2PTR(next);
+	// lo hice para ir viendo cómo va todo
+	print_list();
+
+	return REGION2PTR(region);
 }
 
 void
@@ -118,14 +315,12 @@ free(void *ptr)
 	// updates statistics
 	amount_of_frees++;
 
-	struct region *curr = PTR2REGION(ptr);
-	assert(curr->free == 0);
+	region_t *region_to_free = PTR2REGION(ptr);
+	// assert(region_to_free->free == 0);
 
-	curr->free = true;
+	region_to_free->free = true;
 
-	// Your code here
-	//
-	// hint: maybe coalesce regions?
+	try_coalesce_regions();
 }
 
 void *
--- malloc/test.c
+++ malloc/test.c
@@ -8,18 +8,46 @@
 #define HELLO "hello from test"
 #define TEST_STRING "FISOP malloc is working!"
 
+// IMPLEMENTACION CATEDRA
+//  int
+//  main(void)
+//  {
+//  	printfmt("%s\n", HELLO);
+
+// 	char *var = malloc(100);
+
+// 	strcpy(var, TEST_STRING);
+
+// 	printfmt("%s\n", var);
+
+// 	free(var);
+
+// 	return 0;
+// }
+
+
 int
 main(void)
 {
-	printfmt("%s\n", HELLO);
+	// Test
+	char *var1 = malloc(32);
+	char *var2 = malloc(32);
+	char *var3 = malloc(32);
 
-	char *var = malloc(100);
+	free(var2);
+	char *var4 = malloc(100);
 
-	strcpy(var, TEST_STRING);
+	strcpy(var1, "Test 1");
+	strcpy(var3, "Test 3");
+	strcpy(var4, "Test 4");
 
-	printfmt("%s\n", var);
+	printfmt("%s\n", var1);
+	printfmt("%s\n", var3);
+	printfmt("%s\n", var4);
 
-	free(var);
+	free(var1);
+	free(var3);
+	free(var4);
 
 	return 0;
 }
